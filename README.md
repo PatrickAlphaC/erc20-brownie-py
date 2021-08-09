f4a35f19eb74d890f34124d8c2141bca8a35efbb
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-08-09 18:42:02 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+A repo modified for freecodecamp.
+
+You can see another [version of this repo here.](https://github.com/PatrickAlphaC/erc20-brownie)
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,20 @@
+from brownie import accounts, network, config
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
+    "development",
+    "ganache",
+    "hardhat",
+    "local-ganache",
+    "mainnet-fork",
+]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        print(accounts[0].balance())
+        return accounts[0]
+    if id:
+        return accounts.load(id)
+    return accounts.add(config["wallets"]["from_key"])
